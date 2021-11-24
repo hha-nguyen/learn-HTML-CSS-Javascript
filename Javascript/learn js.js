a9b21e28db47e9385dcd9a37bcc8c03011d1bae1
unknown <xamlol2982002@gmail.com>
2021-11-24 22:19:31 +0700

    add learn js file

--- Javascript/learn js.js
+++ Javascript/learn js.js
@@ -574,12 +574,134 @@ console.log("toString" in Object.create(null)); // → false
 let ages = new Map(); 
 ages.set("Boris", 39); 
 ages.set("Liang", 22);
- ages.set("Júlia", 62);
+ages.set("Júlia", 62);
 console.log(`Júlia is ${ages.get("Júlia")}`); // → Júlia is 62 
 console.log("Is Jack's age known?", ages.has("Jack")); // → Is Jack's age known? false 
 console.log(ages.has("toString")); // → false
 console.log({x: 1}.hasOwnProperty("x")); // → true 
 console.log({x: 1}.hasOwnProperty("toString")); // → false
+
+//Polymorphism
+Rabbit.prototype.toString = function() { 
+    return `a ${this.type} rabbit`; 
+};
+//console.log(String(blackRabbit)); 
+// → a black rabbit
+
+//Symbols
+let sym = Symbol("name"); 
+console.log(sym == Symbol("name")); 
+// → false 
+Rabbit.prototype[sym] = 55; 
+//console.log(blackRabbit[sym]); 
+// → 55
+const toStringSymbol = Symbol("toString"); 
+Array.prototype[toStringSymbol] = function() { 
+    return `${this.length} cm of blue yarn`; 
+};
+console.log([1, 2].toString()); 
+// → 1,2 
+console.log([1, 2][toStringSymbol]()); 
+// → 2 cm of blue yarn
+let stringObject = { 
+    [toStringSymbol]() { 
+        return "a jute rope"; 
+    } 
+}; 
+console.log(stringObject[toStringSymbol]()); 
+// → a jute rope
+
+//The iterator interface 
+let okIterator = "OK"[Symbol.iterator](); 
+console.log(okIterator.next()); 
+// → {value: "O", done: false} 
+console.log(okIterator.next()); 
+// → {value: "K", done: false} 
+console.log(okIterator.next()); 
+// → {value: undefined, done: true}
+class Matrix { 
+    constructor(width, height, element = (x, y) => undefined) { 
+        this.width = width; 
+        this.height = height; 
+        this.content = [];
+        for (let y = 0; y < height; y++) { 
+            for (let x = 0; x < width; x++) { 
+                this.content[y * width + x] = element(x, y); 
+            } 
+        }
+    }
+    get(x, y) { return this.content[y * this.width + x];
+    } set(x, y, value) { this.content[y * this.width + x] = value; }
+}
+class MatrixIterator { 
+    constructor(matrix) { 
+        this.x = 0; 
+        this.y = 0; 
+        this.matrix = matrix; 
+    }
+    next() {
+        if (this.y == this.matrix.height) return {done: true};
+        let value = {
+            x: this.x, 
+            y: this.y, 
+            value: this.matrix.get(this.x, this.y)}; 
+            this.x++; 
+            if (this.x == this.matrix.width) { 
+                this.x = 0; 
+                this.y++; 
+            } return {value, done: false};
+    }
+}
+
+Matrix.prototype[Symbol.iterator] = function() { 
+    return new MatrixIterator(this); 
+};
+let matrix = new Matrix(2, 2, (x, y) => `value ${x},${y}`); 
+for (let {x, y, value} of matrix) { 
+    console.log(x, y, value); 
+} 
+// → 0 0 value 0,0 
+// → 1 0 value 1,0 
+// → 0 1 value 0,1 
+// → 1 1 value 1,1
+
+//Getters, setters, and statics 
+let varyingSize = { 
+    get size() { 
+        return Math.floor(Math.random() * 100); 
+    }
+};
+console.log(varyingSize.size); 
+// → 73 
+console.log(varyingSize.size); 
+// → 49
+class Temperature { 
+    constructor(celsius) { 
+        this.celsius = celsius; 
+    } 
+    get fahrenheit() { 
+        return this.celsius * 1.8 + 32; 
+    } set fahrenheit(value) { 
+        this.celsius = (value - 32) / 1.8; 
+    }
+static fromFahrenheit(value) { 
+    return new Temperature((value - 32) / 1.8); 
+}
+}
+let temp = new Temperature(22); 
+console.log(temp.fahrenheit); 
+// → 71.6 
+temp.fahrenheit = 86; 
+console.log(temp.celsius); 
+// → 30
+
+
+
+    
+
+
+
+
 
 
 
