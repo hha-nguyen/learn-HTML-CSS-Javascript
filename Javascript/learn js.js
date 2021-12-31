08a9453b29ecf5db8776f912abe90dc5bb62a40e
unknown <xamlol2982002@gmail.com>
2021-12-31 18:09:28 +0700

    Update learn Js file

--- Javascript/learn js.js
+++ Javascript/learn js.js
@@ -941,6 +941,63 @@ console.log(reliableMultiply(8, 8));
 // → 64
 //The locked box 
 
+//REGULAR EXPRESSIONS
+//Creating a regular expression
+let re1 = new RegExp("abc"); 
+let re2 = /abc/;
+let eighteenPlus = /eighteen\+/;
+
+//Testing for matches
+console.log(/abc/.test("abcde")); // → true 
+console.log(/abc/.test("abxde")); // → false
+
+//Sets of characters 
+console.log(/[0123456789]/.test("in 1992")); // → true
+console.log(/[0-9]/.test("in 1992")); // → true
+/*
+\d Any digit character 
+\w An alphanumeric character (“word character”) 
+\s Any whitespace character (space, tab, newline, and similar) 
+\D A character that is not a digit 
+\W A nonalphanumeric character 
+\S A nonwhitespace character 
+. Any character except for newline 
+*/
+let dateTime = /\d\d-\d\d-\d\d\d\d \d\d:\d\d/; 
+console.log(dateTime.test("01-30-2003 15:20")); 
+// → true 
+console.log(dateTime.test("30-jan-2003 15:20")); 
+// → false
+let notBinary = /[^01]/;
+console.log(notBinary.test("1100100010100110")); 
+// → false 
+console.log(notBinary.test("1100100010200110")); 
+// → true
+
+//Repeating parts of a pattern
+console.log(/'\d+'/.test("'123'")); // → true 
+console.log(/'\d+'/.test("''")); // → false 
+console.log(/'\d*'/.test("'123'")); // → true 
+console.log(/'\d*'/.test("''")); // → true
+let neighbor = /neighbou?r/; 
+console.log(neighbor.test("neighbour")); 
+// → true 
+console.log(neighbor.test("neighbor")); 
+// → true
+let dateTime = /\d{1,2}-\d{1,2}-\d{4} \d{1,2}:\d{2}/; 
+console.log(dateTime.test("1-30-2003 8:45")); 
+// → true
+
+//Grouping subexpressions
+
+
+
+
+
+
+
+
+
 
 
 
