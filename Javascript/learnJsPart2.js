3f87155170cb5730b0eb425394fb6d6b379c4749
unknown <xamlol2982002@gmail.com>
2022-01-07 10:27:50 +0700

    make a programing langugae

--- Javascript/learnJsPart2.js
+++ Javascript/learnJsPart2.js
@@ -33,9 +33,87 @@ console.log(plusOne(4));
 //Circular dependencies
 
 //Asynchronous Programming
-//Asynchronicity
+//Exercises
+//tracking the scalpel
+// "use strict";
+
+// async function locateScalpel(nest) {
+//   let current = nest.name;
+//   for (;;) {
+//     let next = await anyStorage(nest, current, "scalpel");
+//     if (next == current)
+//       return current;
+//     current = next;
+//   }
+// }
+
+// function locateScalpel2(nest) {
+//   function loop(current) {
+//     return anyStorage(nest, current, "scalpel").then(next => {
+//       if (next == current) 
+//         return current;
+//       else
+//         return loop(next);
+//     });
+//   }
+//   return loop(nest.name);
+// }
+
+// locateScalpel(bigOak).then(console.log);
+// // → Butcher Shop
+
+// locateScalpel2(bigOak).then(console.log);
+// // → Butcher Shop
+
+//Building Promise.All
+"use strict";
+
+function Promise_all(promises) {
+  return new Promise((resolve, reject) => {
+    let results = [];
+    let pending = promises.length;
+    
+    // Array of Promises
+    for (let i = 0; i < promises.length; ++i) {
+      promises[i].then(result => {
+        results[i] = result;
+        --pending;
+        if (pending == 0)
+          resolve(results);
+      }).catch(reject);
+    }
+  
+    // `promises` is Empty.
+    if (promises.length == 0)
+      resolve(results);
+  });
+}
+
+// Test code.
+Promise_all([]).then(array => {
+  console.log("This should be []:", array);
+});
+function soon(val) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(val), Math.random() * 500);
+  });
+}
+Promise_all([soon(1), soon(2), soon(3)]).then(array => {
+  console.log("This should be [1, 2, 3]:", array);
+});
+Promise_all([soon(1), Promise.reject("X"), soon(3)])
+  .then(array => {
+    console.log("We should not get here");
+  })
+  .catch(error => {
+    if (error != "X") {
+      console.log("Unexpected failure:", error);
+    }
+  });
 
 
+//Project: A Programming Language
+//Parsing
 
 
 
