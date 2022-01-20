022180253da51afa36d9309eebe37af70acade40
unknown <xamlol2982002@gmail.com>
2022-01-20 16:25:49 +0700

    doing exercise 7 and 8 of node js course

--- Javascript/Node_js/learn_with_TCT/Excercise/6_Sorting/index.js
+++ Javascript/Node_js/learn_with_TCT/Excercise/6_Sorting/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Datastore = require('nedb');
 
 const app = express();
-app.listen(3000, () => console.log('listening at 3000'));
+app.listen(3006, () => console.log('listening at 3006'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
--- Javascript/Node_js/learn_with_TCT/Excercise/6_Sorting/public/logs/logs.js
+++ Javascript/Node_js/learn_with_TCT/Excercise/6_Sorting/public/logs/logs.js
@@ -1,3 +1,52 @@
 getData();
 
-const selfie = [];
+const selfies = [];
+
+document.getElementById('time', (event) => {
+    sortData((a, b) => b.time - a.time);
+});
+
+document.getElementById('mood', event => {
+    sortData((a, b) => b.mood > a.mood ? -1 : 1);
+})
+
+function sortData(compare) {
+    for (let item of selfies) {
+        item.elt.remove();
+    }
+    selfies.sort(compare);
+    for (let item of selfies) {
+        document.body.append(item.elt);
+    }
+}
+
+async function getData() {
+    const response = await fetch('/api');
+    const data = await response.json();
+    
+    for (item of data) {
+        const root = document.createElement('p');
+        const mood = document.createElement('div');
+        const geo = document.createElement('div');
+        const date = document.createElement('div');
+        const image = document.createElement('img');
+
+        mood.textContent = `mood: ${item.mood}`;
+        geo.textContent = `latitude: ${item.lat}°, longitude: ${item.lon}°`;
+        const dateString = new Date(item.timestamp).toLocaleString();
+        date.textContent = dateString;
+        image.src = item.image64;
+        image.alt = 'Mesto face';
+
+        root.append(mood, geo, date, image);
+
+        selfies.push({
+            elt: root,
+            time: item.timestamp,
+            mood: item.mood
+        });
+
+        document.body.append(root);
+    }    
+    console.log(data);
+}
